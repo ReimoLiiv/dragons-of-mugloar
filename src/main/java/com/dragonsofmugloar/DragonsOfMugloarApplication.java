c18b07fa807bacba2909c45cee69aaf579640890
reimoliiv <reimoliiv@solaireresort.com>
2025-12-12 23:22:15 +0200

    Let the game begin

---
+++ src/main/java/com/dragonsofmugloar/DragonsOfMugloarApplication.java
@@ -0,0 +1,13 @@
+package com.dragonsofmugloar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsOfMugloarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonsOfMugloarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dragonsofmugloar/DragonsOfMugloarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonsofmugloar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonsOfMugloarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
